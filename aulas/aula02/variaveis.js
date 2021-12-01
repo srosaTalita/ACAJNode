7607b2ccef8e1b76a5e5383484e36a3b3f2382aa
Talita Rosa <talisrosa4@gmail.com>
2021-12-01 11:59:14 -0300

    add class - js basic operations

---
+++ aulas/aula02/variaveis.js
@@ -0,0 +1,13 @@
+const nome ="Luci";
+const idade = 50;
+const salario = 1598.08;
+
+console.log(nome + idade);
+console.log(salario + idade);
+
+console.log("10" + "20")
+console.log(parseInt("10") + parseInt("20"));
+
+console.log("10" / "20")
+console.log("10" - "20")
+console.log("10" * "20")
